13817e7cf4de43df975641210e06f391255fadf5
manashe <mukwenarobson@gmail.com>
2024-04-10 08:23:16 +0200

    changes added

--- 07-Errors and Exception Handling/simple1.py
+++ 07-Errors and Exception Handling/simple1.py
@@ -1,14 +1,4 @@
-"""
-A very simple script.
-"""
-
-def myfunc():
-    """
-    An extremely simple function.
-    """
-    first = 1
-    second = 2
-    print(first)
-    print(second)
-
-myfunc()
+a = 1
+b = 2
+print(a)
+print(B)
